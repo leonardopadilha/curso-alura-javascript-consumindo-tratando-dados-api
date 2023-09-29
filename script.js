4aa5d7c3b0348795f33e31884bb38a57b982973b
leonardopadilha <leonardopadilha28@gmail.com>
2023-09-29 17:53:09 -0300

    Async Await

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-let = consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
-                        .then(resposta => resposta.json())
-                            .then(r => {
-                                if (r.erro) {
-                                    throw Error('Esse cep não existe!')
-                                } else 
-                                    console.log(r)
-                                })
-                                .catch(erro => console.log(erro))
-                                    .finally(mensagem => console.log('Processamento concluído!'));
-//console.log(consultaCEP);
+async function buscaEndereco() {
+    let = consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/');
+
+    let consultaCEPConvertida = await consultaCEP.json();
+
+    console.log(consultaCEPConvertida)
+}
+
+buscaEndereco();
