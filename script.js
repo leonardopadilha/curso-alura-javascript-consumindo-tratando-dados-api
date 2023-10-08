1ef431bfa201b45cc1c3944bdfc9d992141d9904
leonardopadilha <leonardopadilha28@gmail.com>
2023-10-08 09:13:47 -0300

    Interagindo com o DOM

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 async function buscaEndereco(cep) {
+
+    let mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+
     try {
         let = consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         let consultaCEPConvertida = await consultaCEP.json();
@@ -7,10 +11,22 @@ async function buscaEndereco(cep) {
             throw Error ('CEP não existente')
         }
 
+        let bairro = document.getElementById('bairro');
+        let cidade = document.getElementById('cidade');
+        let logradouro = document.getElementById('endereco');
+        let estado = document.getElementById('estado');
+
+
+        bairro.value = consultaCEPConvertida.bairro;
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+
         console.log(consultaCEPConvertida)
         return consultaCEPConvertida;
 
     } catch(erro) {
+        mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente!</p>`;
         console.log(erro)
     }
 }
